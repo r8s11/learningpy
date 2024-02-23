a0d5552045abd2ac90c6da30bf34ca2af6d7e4f1
R8S <Roosevelt.bseg@gmail.com>
2024-02-22 22:36:38 -0500

    ch4 first test

--- dfb/Ch4 message-board/posts/tests.py
+++ dfb/Ch4 message-board/posts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Post
+
+class PostsModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Post.objects.create(text="just a test")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f"{post.text}"
+        self.assertEqual(expected_object_name, "just a test")
+
+
 
 # Create your tests here.
