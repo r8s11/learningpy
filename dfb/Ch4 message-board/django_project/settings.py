a8fe09dcdb8be8b787edf5adc1e4928c2d493e47
R8S <Roosevelt.bseg@gmail.com>
2024-02-23 23:56:13 -0500

    ch4 end

--- dfb/Ch4 message-board/django_project/settings.py
+++ dfb/Ch4 message-board/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n)px)_30$4bze$iu1q9c=urmwyb)dj)pzsud(lk8wl*2aahnak
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "posts.apps.PostsConfig",
 ]
 
@@ -56,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [str(BASE_DIR.joinpath('templates'))],
+        "DIRS": [str(BASE_DIR.joinpath("templates"))],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
