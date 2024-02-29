6a21d94ef56246816bf2cca81af90bea2c5fe6b3
R8S <Roosevelt.bseg@gmail.com>
2024-02-28 20:06:56 -0500

    Aliens - finish the first step

--- pcc/alien_invasion/alien_invasion.py
+++ pcc/alien_invasion/alien_invasion.py
@@ -2,14 +2,21 @@ import sys
 import pygame
 
 class AliensInvation:
-    """Initialze the game, and create game resources."""
-    pygame.init()
+    def __init__(self):
+        """Initialze the game, and create game resources."""
+        pygame.init()
 
-    self.screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
-        print("hello")
+        """Start the man loop for the game"""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
