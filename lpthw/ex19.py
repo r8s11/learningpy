a3e041270d8ee9b2232d6a901562087d3dd90569
R8S <Roosevelt.bseg@gmail.com>
2024-03-09 17:57:28 -0500

    Started ex19.py

---
+++ lpthw/ex19.py
@@ -0,0 +1,11 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f"You have {cheese_count} cheese!")
+    print(f'You have {boxes_of_crackers} boxes of crackers!')
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+
+print ( "We can just give the function number directly:")
+cheese_and_crackers(20, 30)
+
+print("OR, we can use variables from our script:")
+
